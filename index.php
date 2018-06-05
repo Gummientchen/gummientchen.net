74f1b906c64bcd2b1b539db0d4194a19b2571a63
Stefan Blatter <sbl@BRIENZ.LOCAL>
2018-06-05 15:08:30 +0200

    integrated dwoo template

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$content = file_get_contents('index.max.html');
+$dwoo = new Dwoo\Core();
+$tpl = new Dwoo\Template\File('index.max.tpl');
+
+$data = new Dwoo\Data();
+// Fill it with some data
+$data->assign('foo', 'BAR');
+$data->assign('bar', 'BAZ');
+
+$content = $dwoo->get($tpl, $data);
 $cssMain = file_get_contents('css/main.css');
 
 $content = str_replace("%mainCSS%", $cssMain, $content);
